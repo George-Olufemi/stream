97c27ea0765796914984fa00d45cd29e07dba817
George Olufemi <georgeoluolufemi@gmail.com>
2023-01-14 15:37:51 +0100

    v1 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { StreamChat } from 'stream-chat';
+import { 
+  Chat,
+  Channel,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  LoadingIndicator
+} from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
+
+  const apiKey = process.env.REACT_APP_STREAM_API_KEY
+
+  const user = {
+    id: "george",
+    name: "george",
+    image: "/logo512.png"
+  }
 
 function App() {
+  const [client, setClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      const chatClient = StreamChat.getInstance(apiKey)
+      await chatClient.connectUser(user, chatClient.devToken(user.id))
+      const channel = chatClient.channel('messaging', 'react-talk', {
+        image: '/logo512.png',
+        name: 'Gist',
+        members: [user.id]
+      }) 
+      await channel.watch()
+      setChannel(channel)
+      setClient(chatClient)
+    }
+    init()
+    if (client) return () => client.disconnectUser()
+  }, [])
+
+  if (!channel || !client) return <LoadingIndicator />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client} theme='messaging light'>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
 }
 
